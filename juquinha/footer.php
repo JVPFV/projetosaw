bf0443e9d02d7b073cb47dcd8e24fa0f3222d4b8
JVPFV <99732249+JVPFV@users.noreply.github.com>
2022-05-30 16:07:12 -0300

    Add files via upload

---
+++ juquinha/footer.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+	<?php wp_footer(); ?>
+
+	<div class="row">
+		<div class="col-md-12 rodape">
+            <center>
+            <br><br><p>Site desenvolvido por Juquinha do leite AKA João Victor 3MIN &nbsp; &nbsp; <img src="<?php echo get_template_directory_uri()?>/assets/img/logo.png" width="221" height="55"></p>
+            </center>
+		</div>
+	</div>
+	
+</body>
+</html>
